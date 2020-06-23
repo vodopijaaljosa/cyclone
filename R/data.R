729c639735f85e8644417c0c5c6faa64cdcfa3b0
aljosa.vodopija <aljosa.vodopija@ijs.si>
2020-06-23 15:47:13 +0200

    added data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' CMOP bounds
+#'
+#' The dataset containing bounds of CMOPs.
+#'
+#' @format A list of 3 CMOPs and 6 variables representing cyclone's geometrical parameters (Da, Dt, H, Ht, He, Be)
+#'
+"cmops"
